cf0a8180387100953d475ccb34177f176fa64f27
dmusan <dim16@ic.ac.uk>
2020-05-06 13:59:22 +0300

    Refactor App.js and extract web3 component

--- src/App.js
+++ src/App.js
@@ -1,90 +1,18 @@
 import React, { Component } from 'react'
-import Web3 from 'web3'
 import './App.css'
-import crtInterface from './interface.json'
 import { BrowserRouter } from 'react-router-dom'
-
-import IMG_001 from './assets/img/001.png'
-import IMG_004 from './assets/img/004.png'
-import IMG_007 from './assets/img/007.png'
 
 import Navbar from './components/layout/navbar/Navbar'
 
-import Button from '@material-ui/core/Button';
-
-const CONTRACT_ADDRESS = '0xf98b18f2b4F5EEB07cD1FE5E93936234cbEa023E'
-const IMGS = [IMG_001, IMG_004, IMG_007]
-
-
-async function getAccounts() {
-  return window.ethereum.enable()
-}
-
-async function getCards(web3, account) {
-  const crt = new web3.eth.Contract(crtInterface, CONTRACT_ADDRESS, { from: account })
-  const totalCards = parseInt(await crt.methods.totalCards().call())
-
-  return Promise.all([...Array(totalCards).keys()].map(
-    id => crt.methods.cards(id).call()
-  ))
-}
 
 class App extends Component {
-  state = {
-    cards: [],
-  };
-
-  componentDidMount() {
-    this._getAccounts = getAccounts().then(
-      accounts => {
-        this._getAccounts = null;
-        this._web3 = new Web3(window.ethereum);
-        this._account = accounts[0];
-        this.refreshCards()
-      }
-    );
-  }
-
-  componentWillUnmount() {
-    if (this._getAccounts) {
-      this._getAccounts.cancel();
-    }
-
-    if (this._getCards) {
-      this._getCards.cancel();
-    }
-  }
-
-  refreshCards() {
-    this._getCards = getCards(this._web3, this._account).then(
-      _cards => {
-        this._getCards = null
-        this.setState({cards: _cards})
-      }
-    )
-  }
-
-  buyCard(id, price) {
-    const crt = new this._web3.eth.Contract(crtInterface, CONTRACT_ADDRESS, {from: this._account})
-    console.log(crt.options)
-    crt.methods.buyCard(id).send({value: price+1}).on('confirmation', () => {
-      this.refreshCards();
-    })
-  }
 
   render() {
-    const cards = this.state.cards.map(card =>
-      // <p>{card.price} {card.owner} {card.species}</p>
-      <figure>
-        <img src={IMGS[parseInt(card.species)]} />
-        <figcaption>Price: {card.price} | Owner: {card.owner} | <button onClick={()=> this.buyCard(card.id, parseInt(card.price))}>Buy</button></figcaption>
-      </figure>
-    )
     return (
       <BrowserRouter>
         <Navbar />
-        <div className="container">
-          {cards}
+        <div className="App">
+
         </div>
       </BrowserRouter>
     );
--- src/components/layout/navbar/Navbar.js
+++ src/components/layout/navbar/Navbar.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import NavbarLinks from './NavbarLinks'
+
 const Navbar = () => {
   return (
     <nav className="nav-wrapper grey darken-3">
       <div className="container">
-        <Link to='/' className="brand-logo">DOTC.market</Link>
+        <Link to='/' className="brand-logo left-align">DOTC.market</Link>
         <NavbarLinks/>
       </div>
     </nav>

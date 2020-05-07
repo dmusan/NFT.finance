3d1a12dbcb936ad578af9fbe11deb777f4d75ac6
dmusan <dim16@ic.ac.uk>
2020-05-07 14:00:03 +0300

    Update NewOrderCard

--- src/components/layout/neworder/NewOrderCard.js
+++ src/components/layout/neworder/NewOrderCard.js
@@ -10,15 +10,53 @@ const NewOrderCard = () => {
             <span class="card-title">New Order</span>
             <br/>
             <p>
-              Specify the details of your loan:
+              Specify the details of your loan
             </p>
             <br/>
           </div>
-          <div class="row">
-            <div class="input-field col s6 m6 offset-s1 offset-m1">
-              <input id="email" type="email" class="validate" />
-              <label for="email">Email</label>
-            </div>
+          <div class="row white-text">
+            <form class="col s12 m12">
+              <div class="row">
+                <div class="row">
+                  <div class="col s12">
+                    Choose your collateral:
+                    <div class="input-field inline">
+                      <input id="collateral_id" type="text" class="validate" />
+                      <label for="collateral_id">Collateral</label>
+                      <span class="helper-text" data-error="wrong" data-success="right">You can choose collateral from your wallet</span>
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col s12">
+                    Loan Amount:
+                    <div class="input-field inline">
+                      <input id="loan_amount" type="text" class="validate" />
+                      <label for="loan_amount">Amount</label>
+                      <span class="helper-text" data-error="wrong" data-success="right">How much you would like to receive as loan</span>
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col s12">
+                    Monthly interest:
+                    <div class="input-field inline">
+                      <input id="monthly_interest" type="text" class="validate" />
+                      <label for="monthly_interest">Percentage</label>
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col s12">
+                    Total Loan Period:
+                    <div class="input-field inline">
+                      <input id="loan_period_months" type="text" class="validate" />
+                      <label for="loan_period_months">Months</label>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </form>
           </div>
           <div class="card-action">
             <NavLink to='/neworder'>Place new order</NavLink>

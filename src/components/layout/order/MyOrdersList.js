056c0931c580e7cccd41a9577518aea1b1135c4d
dmusan <dim16@ic.ac.uk>
2020-05-07 18:08:17 +0300

    Add collapsable components to orders

--- src/components/layout/order/MyOrdersList.js
+++ src/components/layout/order/MyOrdersList.js
@@ -1,35 +1,14 @@
-import React, { Component } from 'react'
+import React from 'react'
 import SingleOrder from './SingleOrder'
-import M from "materialize-css";
-import "materialize-css/dist/css/materialize.min.css";
 
-
-class MyOrdersList extends Component {
-
-  componentDidMount() {
-    const options = {
-      duration: 300,
-      onCycleTo: () => {
-        console.log("New Slide");
-      }
-    };
-
-    document.addEventListener('DOMContentLoaded', function() {
-      var elems = document.querySelectorAll('.collapsible');
-      var instances = M.Collapsible.init(elems, options);
-    });
-  }
-
-  render() {
-    return (
-      <ul class="collapsible">
-        <SingleOrder />
-        <SingleOrder />
-        <SingleOrder />
-      </ul>
-    )
-  }
-
+const MyOrdersList = () => {
+  return (
+    <div>
+      <SingleOrder />
+      <SingleOrder />
+      <SingleOrder />
+    </div>
+  )
 }
 
 export default MyOrdersList;
--- src/components/layout/order/SingleOrder.js
+++ src/components/layout/order/SingleOrder.js
@@ -1,12 +1,41 @@
-import React from 'react'
+import React, { Component } from 'react'
+import M from "materialize-css";
 
-const SingleOrder = () => {
+class SingleOrder extends Component {
+  componentDidMount() {
+    const options = {
+      duration: 300,
+      onCycleTo: () => {
+        console.log("New Slide");
+      }
+    };
+
+    function onReady() {
+      var elems = document.querySelectorAll('.collapsible');
+      var instances = M.Collapsible.init(elems, options);
+    }
+
+
+    if (document.readyState !== "loading") {
+      onReady(); // Or setTimeout(onReady, 0); if you want it consistently async
+    } else {
+      document.addEventListener("DOMContentLoaded", onReady);
+    }
+
+  }
+
+  render() {
+
   return (
+    <ul class="collapsible">
+
     <li>
-      <div class="collapsible-header"><i class="material-icons">filter_drama</i>First</div>
-      <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
-    </li>
+  <div class="collapsible-header"><i class="material-icons">place</i>Third</div>
+  <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+</li>
+</ul>
   )
+}
 }
 
 export default SingleOrder;

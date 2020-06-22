fb8211ec67d2cc310518dee77fb4ff8061ba0ed3
dmusan <dim16@ic.ac.uk>
2020-06-22 11:43:05 +0300

    New Loan update

--- src/components/layout/loans/newloan/NewLoanRequestModalContent.js
+++ src/components/layout/loans/newloan/NewLoanRequestModalContent.js
@@ -1,16 +1,11 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Button, Blockie } from "rimble-ui";
-import { EthAddress } from "rimble-ui";
+import { Button, Blockie, EthAddress } from "rimble-ui";
 
-import '../../../../css/mystyles.css'
-import { approveNFT } from '../../../../services/web3/loansNFTContract'
-import { createLoanRequest } from '../../../../services/web3/loansNFTContract'
+import { approveNFT, createLoanRequest } from '../../../../services/web3/loansNFTContract'
 
-// TODO: Rename to NewLeaseOfferModalContent
 class NewLoanRequestModalContent extends Component {
 
-    // State will get be filled with changes to input components
     state = {
         loanAmount: '',
         interestAmount: '',
@@ -47,57 +42,54 @@ class NewLoanRequestModalContent extends Component {
     render() {
       return (
         <div className="container">
-          <h5 className="card-asset-content">
-            <p>Specify the details for your loan request.</p>
-            <p>Your selected NFT will be used as collateral.</p>
-          </h5>
+          <h4 className="card-asset-content indigo-text text-darken-2 card-asset-content">
+            New Loan Request with your NFT as collateral
+          </h4>
           <div className="left-align">
-            <p><b className="input-margins">NFT name:</b> {this.props.nft.name}</p>
+            <p><b className="input-margins">NFT name</b></p><p> {this.props.nft.name}</p>
             <div className="row">
-              <div className="col s1 m1">
-                <b>NFT address: </b>
+              <div className="col s6 m6">
+                <b>NFT address</b>
               </div>
-              <div className="col s1 m1">
-              </div>
-              <div className="col s10 m10 right-align">
+              <div className="col s12 m12 right-align">
                 <EthAddress address={this.props.nft.asset_contract.address} />
               </div>
             </div>
             <form>
               <div>
-                <b className="input-margins">Loan Amount:</b>
-                <div class="input-field inline">
+                <b className="input-margins">Loan Amount</b>
+                <div className="input-field">
                   <input id="loan_amount" type="text" name="loanAmount" onChange={this.handleChange} />
                   <label htmlFor="loan_amount">Amount in ETH</label>
                 </div>
               </div>
               <div>
-                <b className="input-margins">Interest Amount:</b>
-                <div class="input-field inline">
+                <b className="input-margins">Interest Amount</b>
+                <div className="input-field">
                   <input id="interest_amount" type="text" name="interestAmount" onChange={this.handleChange} />
                   <label htmlFor="interest_amount">Amount in ETH</label>
                 </div>
               </div>
               <div>
-                <b className="input-margins">Length of one interest period:</b>
-                <div class="input-field inline">
+                <b className="input-margins">Length of one interest period</b>
+                <div className="input-field">
                   <input id="lease_period" type="text" name="singlePeriodTime" onChange={this.handleChange} />
                   <label htmlFor="lease_period">Days for one period</label>
                 </div>
               </div>
               <div>
-                <b className="input-margins">Maximum number of interest periods:</b>
-                <div class="input-field inline">
+                <b className="input-margins">Maximum number of interest periods</b>
+                <div className="input-field">
                   <input id="interest_periods" type="text" name="maximumInterestPeriods" onChange={this.handleChange} />
                   <label htmlFor="interest_periods">Max</label>
                 </div>
               </div>
             </form>
             <div className="row">
-                <button className="btn indigo lighten-1 button-offer" onClick={ this.approveNFT }>1. Approve Transfer</button>
+                <button className="btn indigo lighten-1 button-offer right" onClick={ this.approveNFT }>1. Approve Transfer</button>
             </div>
             <div className="row">
-                <button className="btn indigo lighten-1 button-offer" onClick={ this.newLoanRequest }>2. Create Offer</button>
+                <button className="btn indigo lighten-1 button-offer right" onClick={ this.newLoanRequest }>2. Create Offer</button>
             </div>
           </div>
         </div>
--- src/components/layout/loans/newloan/NewLoanRequestPage.js
+++ src/components/layout/loans/newloan/NewLoanRequestPage.js
@@ -4,9 +4,10 @@ import MyAssetsCards from '../../cards/MyAssetsCards'
 const NewLoanRequestPage = () => {
   return (
     <div className="container">
-      <h5 className="grey-text text-darken-3 page-title-margin">
-      Pick an NFT from your wallet as collateral for the loan
-      </h5>
+      <h4 className="indigo-text text-darken-2 page-title-margin">
+      Create New Loan Request
+      </h4>
+      <p className="grey-text text-darken-2 page-subtitle-margin">A Loan Request enables you to get access to liquidity by locking your NFT as collateral</p>
       <MyAssetsCards type="loan"/>
     </div>
   )

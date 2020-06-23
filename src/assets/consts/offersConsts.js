65a1cb44d1f76991a391e3960c4f622c793f25ab
dmusan <dim16@ic.ac.uk>
2020-06-23 10:12:30 +0300

    Update icon

--- src/assets/consts/offersConsts.js
+++ src/assets/consts/offersConsts.js
@@ -1,3 +1,5 @@
 export const OFFER_STATUS = ["Pending", "Active", "Cancelled", "Ended"];
 
 export const LEASING_CONTRACT_ADDRESS = '0xf35515FeBFFe6646Efe262a44AB1888E8B90DD8A';
+
+export const RINKEBY_NETWORK_VERSION = '4';
